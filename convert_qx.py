cf0e9b20ef609192eab61f4d3de60f729c06bda5
1009476063 <1009476063@qq.com>
2026-06-22 15:00:34 +0800

    Fix QuantumultX format with proper parameters

--- convert_qx.py
+++ convert_qx.py
@@ -18,72 +18,67 @@ for proxy in data.get('proxies', []):
     flow = proxy.get('flow', '')
     
     if proxy_type == 'vless':
-        obfs = ''
+        # QuantumultX vless format
+        obfs_params = []
         if tls:
-            obfs = f'tls'
+            obfs_params.append('over-tls=true')
             if servername:
-                obfs += f',host={servername}'
-        
-        params = []
-        if skip_cert_verify:
-            params.append('skip-cert-verify=1')
+                obfs_params.append(f'tls-host={servername}')
+            obfs_params.append('tls-verification=true' if not skip_cert_verify else 'tls-verification=false')
         if flow and 'xtls' in flow:
-            params.append('xtls-params=xray')
-        if obfs:
-            params.append(obfs)
+            obfs_params.append('tls-params=xtls')
         
-        param_str = ','.join(params)
+        obfs_str = ', '.join(obfs_params)
         line = f'{name} = vless, {server}, {port}, {uuid}, {cipher}'
-        if param_str:
-            line += f', {param_str}'
+        if obfs_str:
+            line += f', {obfs_str}'
         qx_lines.append(line)
     elif proxy_type == 'vmess':
-        obfs = ''
+        # QuantumultX vmess format
+        obfs_params = []
         if tls:
-            obfs = f'tls'
+            obfs_params.append('over-tls=true')
             if servername:
-                obfs += f',host={servername}'
+                obfs_params.append(f'tls-host={servername}')
+            obfs_params.append('tls-verification=true' if not skip_cert_verify else 'tls-verification=false')
+        if proxy.get('network') == 'ws':
+            obfs_params.append('obfs=ws')
+            obfs_path = proxy.get('ws-opts', {}).get('path', '')
+            if obfs_path:
+                obfs_params.append(f'obfs-uri={obfs_path}')
         
-        params = []
-        if skip_cert_verify:
-            params.append('skip-cert-verify=1')
-        if obfs:
-            params.append(obfs)
-        
-        param_str = ','.join(params)
+        obfs_str = ', '.join(obfs_params)
         line = f'{name} = vmess, {server}, {port}, {cipher}, {uuid}'
-        if param_str:
-            line += f', {param_str}'
+        if obfs_str:
+            line += f', {obfs_str}'
         qx_lines.append(line)
     elif proxy_type == 'ss':
         method = proxy.get('cipher', 'aes-256-gcm')
         password = proxy.get('password', '')
-        obfs = ''
-        obfs_opts = proxy.get('plugin-opts', {})
-        if proxy.get('plugin') == 'obfs':
-            obfs = f'obfs={obfs_opts.get("mode", "http")}'
-            if obfs_opts.get('host'):
-                obfs += f',obfs-host={obfs_opts["host"]}'
+        obfs_params = []
+        plugin = proxy.get('plugin', '')
+        plugin_opts = proxy.get('plugin-opts', {})
+        if plugin == 'obfs':
+            obfs_params.append(f'obfs={plugin_opts.get("mode", "http")}')
+            if plugin_opts.get('host'):
+                obfs_params.append(f'obfs-host={plugin_opts["host"]}')
         
+        obfs_str = ', '.join(obfs_params)
         line = f'{name} = ss, {server}, {port}, encrypt-method={method}, password={password}'
-        if obfs:
-            line += f', {obfs}'
+        if obfs_str:
+            line += f', {obfs_str}'
         qx_lines.append(line)
     elif proxy_type == 'trojan':
-        obfs = f'tls'
+        obfs_params = []
+        obfs_params.append('over-tls=true')
         if servername:
-            obfs += f',host={servername}'
+            obfs_params.append(f'tls-host={servername}')
+        obfs_params.append('tls-verification=true' if not skip_cert_verify else 'tls-verification=false')
         
-        params = []
-        if skip_cert_verify:
-            params.append('skip-cert-verify=1')
-        if obfs:
-            params.append(obfs)
-        
-        param_str = ','.join(params)
+        obfs_str = ', '.join(obfs_params)
         line = f'{name} = trojan, {server}, {port}, {uuid}'
-        if param_str:
-            line += f', {param_str}'
+        if obfs_str:
+            line += f', {obfs_str}'
         qx_lines.append(line)
 
 print('\n'.join(qx_lines))
